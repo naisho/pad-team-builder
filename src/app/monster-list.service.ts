73688d3b9647fa0e082ccd53fa44f16dce9aeb55
naisho <vertigodragon@gmail.com>
2018-02-24 15:42:05 -0500

    changed structure to subscribe from app.component

--- src/app/monster-list.service.ts
+++ src/app/monster-list.service.ts
@@ -8,7 +8,6 @@ import { SortOption } from './shared/sortOption';
 import { Monster } from './shared/monster';
 
 import { Observable } from 'rxjs/Observable';
-// import { BehaviorSubject } from 'rxjs';
 
 import 'rxjs/add/observable/of';
 import 'rxjs/add/operator/map';
@@ -21,30 +20,21 @@ export class MonsterListService {
 		) {	}
 
 	monsterList$: Observable<Monster[]> = this.dataSource.monsterList$
-		.map(r => this.truncateProps(r))
-		.map(r => this.filterPADObjects(r, this.Filters.isMaxLevel))
-		.map(r => r.sort(this.SortOption.totalSort))
+		.map( r => this.truncateProps(r) )
+		.map( r => r.filter((mon) => mon.isMaxLevel()) )
+		// .map(r => r.sort(this.SortOption.totalSort))
 
 	private truncateProps(originalResponse: Monster[]) {
 		let propertyList = ["id","name","element","element2","type","type2","type3","hp_max","atk_max","rcv_max","max_level","awoken_skills","image40_href","image60_href"]
 		let monsterList: Monster[] = [];
 
-		for (let mon in originalResponse) {
+		// cannot use for...in because of extended array monster-array.ts
+		originalResponse.forEach((monster, index) => {
 			monsterList.push(new Monster);
-			let current = monsterList[mon];
-		
 			for (let prop of propertyList) {
-				current[prop] = originalResponse[mon][prop];
+				monsterList[index][prop] = monster[prop];
 			} // for-prop
-		} // for-mon
-	
+		}); // for-mon
 		return monsterList;
 	} // getRelevantProps
-
-	private filterPADObjects(monsterList: Monster[], filter: (monster) => boolean) {
-		return monsterList.filter(filter);
-	} // filterPADObjects
-
-
-
 }
